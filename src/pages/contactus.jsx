de579738d49247ea050c8a33a2ffc7a2490c09fc
Gourav Goyal <gouravgoyal290@gmail.com>
2020-06-05 17:01:30 +0530

    Page Contactus

---
+++ src/pages/contactus.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Contact from '../components/contact'
+import Footer from '../components/footer'
+
+const contactus = () => {
+ return(
+     <div>
+         <Contact />
+         <Footer />
+     </div>
+ )
+}
+export default contactus
---
+++ src/pages/feature.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Features from '../components/features'
+import Footer from '../components/footer'
+
+const feature = () => {
+    
+    return (
+        <div>
+            <Features />
+            <Footer />
+        </div>
+    )
+}
+export default feature
